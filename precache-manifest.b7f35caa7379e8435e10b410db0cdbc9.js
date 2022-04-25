e7a23341cee49e2e6a9c2729f522428e991491c8
TuhsarChaudhary <97732727+TuhsarChaudhary@users.noreply.github.com>
2022-04-25 07:44:29 +0530

    Updates

---
+++ precache-manifest.b7f35caa7379e8435e10b410db0cdbc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49d4273d47369f54995b",
+    "url": "/textutils/static/css/main.466b393a.chunk.css"
+  },
+  {
+    "revision": "49d4273d47369f54995b",
+    "url": "/textutils/static/js/main.49d4273d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/textutils/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "56ed422ec3b1839cedd513547d01399f",
+    "url": "/textutils/index.html"
+  }
+];
